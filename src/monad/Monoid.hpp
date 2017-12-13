f976fc20f880437f83cd4e58198584741f3885e0
rabenda <a936902017@gmail.com>
2017-12-13 17:51:05 +0800

    Add Monad Module and update CMake
    Signed-off-by: rabenda <a936902017@gmail.com>

---
+++ src/monad/Monoid.hpp
@@ -0,0 +1,28 @@
+//
+// Created by Rabenda on 13/12/2017.
+//
+
+#ifndef RABENDALIB_MONOID_HPP
+#define RABENDALIB_MONOID_HPP
+
+#include "../config.h"
+
+
+NAMESPACE_BEGIN(MONAD)
+	
+	template<class T>
+	struct Monoid {
+		static T mempty() {
+			return T{};
+		}
+	};
+	
+	template<class T>
+	static T mempty() {
+		return Monoid<T>::mempty();
+	}
+
+NAMESPACE_END(MONAD)
+
+
+#endif //RABENDALIB_MONOID_HPP
---
+++ src/monad/Semigroup.hpp
@@ -0,0 +1,28 @@
+//
+// Created by Rabenda on 13/12/2017.
+//
+
+#ifndef RABENDALIB_SEMIGROUP_HPP
+#define RABENDALIB_SEMIGROUP_HPP
+
+#include "../config.h"
+
+NAMESPACE_BEGIN(MONAD)
+	
+	
+	template<class T>
+	struct Semigroup {
+		static T append(T const &a, T const &b) {
+			return a + b;
+		}
+	};
+	
+	template<class T>
+	static T append(T const &a, T const &b) {
+		return Semigroup<T>::append(a, b);
+	}
+
+NAMESPACE_END(MONAD)
+
+
+#endif //RABENDALIB_SEMIGROUP_HPP
